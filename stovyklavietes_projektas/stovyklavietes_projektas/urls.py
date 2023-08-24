33081b6ca460f19e138ab9c8acbfa08a6b7c127d
Aidas8 <aidas.liudvinavicius@gmail.com>
2023-08-24 20:31:48 +0300

    siek tiek pataisymu

--- stovyklavietes_projektas/stovyklavietes_projektas/urls.py
+++ stovyklavietes_projektas/stovyklavietes_projektas/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, add_stovyklaviete, info, about
+from stovyklavietes.views import index, add_stovyklaviete, info, about
 
 urlpatterns = [
     path('', index, name='index'),
