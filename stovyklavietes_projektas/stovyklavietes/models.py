f95e5b390ce9813cb9dff718bb76f2612a63db67
Aidas8 <aidas.liudvinavicius@gmail.com>
2023-08-31 21:44:28 +0300

    dar siek tiek pataisymu ir vazuojam toliau

--- stovyklavietes_projektas/stovyklavietes/models.py
+++ stovyklavietes_projektas/stovyklavietes/models.py
@@ -3,7 +3,9 @@ from django.db import models
 class Stovyklaviete(models.Model):
     pavadinimas = models.CharField(max_length=100)
     reitingas = models.PositiveIntegerField()
-
+    koordinates = models.CharField(max_length=50, blank=True, null=True)   # Pridetas koordinaciu laukas
+    nuotrauka = models.ImageField(upload_to='stovyklavietes/', blank=True, null=True)             # Pridetas nuotrauku laukas
+    papildoma_info = models.TextField(blank=True, null=True)                                    # Pridetas papildoma_info laukas
     def __str__(self):
         return self.pavadinimas
 
