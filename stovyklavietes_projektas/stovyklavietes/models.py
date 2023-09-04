e0dee066cd4fcc0e783ddadf12105324fbe68922
Aidas8 <aidas.liudvinavicius@gmail.com>
2023-09-04 22:09:43 +0300

    patobulintas variantas (su daugiau funcionalumo ir veikiantis)

--- stovyklavietes_projektas/stovyklavietes/models.py
+++ stovyklavietes_projektas/stovyklavietes/models.py
@@ -1,13 +1,35 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+REGIONAI = (
+    ('Aukstaitija', 'Aukštaitija'),
+    ('Dzukija', 'Dzūkija'),
+    ('Suvalkija', 'Suvalkija'),
+    ('Zemaitija', 'Žemaitija'),
+)
 
 class Stovyklaviete(models.Model):
     pavadinimas = models.CharField(max_length=100)
-    reitingas = models.PositiveIntegerField()
-    koordinates = models.CharField(max_length=50, blank=True, null=True)   # Pridetas koordinaciu laukas
-    nuotrauka = models.ImageField(upload_to='stovyklavietes/', blank=True, null=True)             # Pridetas nuotrauku laukas
-    papildoma_info = models.TextField(blank=True, null=True)                                    # Pridetas papildoma_info laukas
+    reitingas = models.PositiveIntegerField(                 #reitingavimo kodas
+        default=1,
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10),
+        ]
+    )
+    koordinates = models.CharField(max_length=50, blank=True, null=True)  #prideja "blank" ir "null=true" galime palikti tuscia lauka
+    nuotrauka = models.ImageField(upload_to='stovyklavietes/', blank=True, null=True)
+    papildoma_info = models.TextField(blank=True, null=True)
+    regionas = models.CharField(                         #galimybe rinktis regiona
+        max_length=20,
+        choices=[('Rinkis', 'Rinkis')] + list(REGIONAI),
+        default='Rinkis',
+    )
+    vieta = models.CharField(max_length=100, blank=True, null=True)  # Pridedame lauką "vieta"
+
     def __str__(self):
         return self.pavadinimas
+
 
 class Privalumas(models.Model):
     pavadinimas = models.CharField(max_length=50)
--- stovyklavietes_projektas/stovyklavietes/views.py
+++ stovyklavietes_projektas/stovyklavietes/views.py
@@ -9,11 +9,21 @@ def add_stovyklaviete(request):
     if request.method == 'POST':
         pavadinimas = request.POST['pavadinimas']
         reitingas = int(request.POST['reitingas'])
+        vieta = request.POST['vieta']
+        regionas = request.POST['regionas']
         koordinates = request.POST['koordinates']
         nuotrauka = request.FILES.get('nuotrauka')
         papildoma_info = request.POST['papildoma_info']
 
-        stovyklaviete = Stovyklaviete.objects.create(pavadinimas=pavadinimas, reitingas=reitingas, koordinates=koordinates, nuotrauka=nuotrauka, papildoma_info=papildoma_info)
+        stovyklaviete = Stovyklaviete.objects.create(
+            pavadinimas=pavadinimas,
+            reitingas=reitingas,
+            vieta=vieta,
+            regionas=regionas,
+            koordinates=koordinates,
+            nuotrauka=nuotrauka,
+            papildoma_info=papildoma_info
+        )
 
         privalumai = request.POST.getlist('privalumai')
         for privalumas in privalumai:
